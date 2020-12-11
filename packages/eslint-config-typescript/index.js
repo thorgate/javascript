324046f89f153202babbbf852d4f4ad725f650ce
Jorgen Ader <jorgen.ader@gmail.com>
2020-12-11 17:58:03 +0200

    Disable `@typescript-eslint/naming-convention`
    - This is more specific rule so should be defined in projects

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -83,6 +83,7 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': 'off', // warn
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/naming-convention': 'off',
         '@typescript-eslint/no-unused-vars': [
             'warn',
             { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
